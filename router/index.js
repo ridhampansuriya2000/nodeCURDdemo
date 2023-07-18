819672d3ac09569db220ca83eeeb916891626c26
BansiKhokhariya <bansikhokhariya59@gmail.com>
2023-07-18 16:16:49 +0530

    change swagger path

--- router/index.js
+++ router/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const radheKrishna = require('./RadheKrishna');
 const authRouter = require("./Auth");
-const router = express.Router();
 const appDataRouter = require('./AppData');
+const router = express.Router();
 
 /*----------initial HTML page---------*/
 router.use('/', radheKrishna);
@@ -16,11 +16,11 @@ const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
 const path = require('path');
 
-
 // Read the Swagger JSON file
-const filePath = path.join("router", '../swagger.json');
-const swaggerFile = fs.readFileSync(filePath,  { encoding: 'utf8' });
-const swaggerData = JSON.parse(swaggerFile);
+// const filePath = path.join("router", './../swagger.json');
+// const swaggerFile = fs.readFileSync(filePath, 'utf8');
+const data = require('../swagger.json');
+const swaggerData = JSON.parse(JSON.stringify(data));
 
 const options = {
     swaggerOptions: {
